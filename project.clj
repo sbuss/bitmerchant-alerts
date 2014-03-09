d6b3f9199ceab528f6706915239cd560b291f5e4
Steven Buss <steven.buss@gmail.com>
2014-03-08 22:49:18 -0800

    Update description and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject bitmerchant-alerts "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Blockchain event alerts"
+  :url "https://github.com/sbuss/bitmerchant-alerts"
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
