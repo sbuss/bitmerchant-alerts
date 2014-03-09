ee34a64a71252f9818c0ac16fefd3c108389b9f9
Steven Buss <steven.buss@gmail.com>
2014-03-08 22:42:22 -0800

    Create project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to bitmerchant-alerts
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/bitmerchant_alerts/core_test.clj
@@ -0,0 +1,7 @@
+(ns bitmerchant-alerts.core-test
+  (:require [clojure.test :refer :all]
+            [bitmerchant-alerts.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
